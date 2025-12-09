6829f078ea1a86a7030bc812b82ce1e422a0f5af
anthony <bernard5107@naver.com>
2025-12-09 16:09:55 +0900

    Add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py
+
+# Get two numbers
+first = input("Enter the first number: ")
+second = input("Enter the second number: ")
+
+# Change into Integer
+num1 = int(first)
+num2 = int(second)
+
+# Caculate Sum
+result = num1 + num2
+
+# Output the Result
+print("Sum: ",result)
